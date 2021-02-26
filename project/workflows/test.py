f225d924764910d4be026a8bf5fcc64c2c2f3a18
zccaayo <zccaayo@login13.myriad.ucl.ac.uk>
2021-02-26 13:14:04 +0000

    modifications

--- project/workflows/test.py
+++ project/workflows/test.py
@@ -5,38 +5,49 @@ from pymatgen.io.vasp.sets import MPRelaxSet, MPStaticSet
 from fireworks import Firework, Workflow
 from atomate.vasp.fireworks.core import OptimizeFW, TransmuterFW
 from atomate.vasp.firetasks.parse_outputs import GibbsFreeEnergyTask
+from ase.build import fcc111, add_adsorbate # ASE's utilities to build the surface
+from clusterx.parent_lattice import ParentLattice
+from clusterx.structures_set import StructuresSet
+from clusterx.visualization import juview
+from clusterx.super_cell import SuperCell
+from random import randint
 
-
-def wf_gibbs_free_energy(structure, deformations, vasp_input_set_relax=None, vasp_input_set_static=None, vasp_cmd="vasp",
-                             db_file=None, user_kpoints_settings=None, t_step=10, t_min=0, t_max=1000,
-                             mesh=(20, 20, 20), eos="vinet", qha_type="debye_model", pressure=0.0,
-                             poisson=0.25):
+def wf_clusterx(structure_prim, vasp_input_set_relax=None, vasp_input_set_static=None, vasp_cmd="vasp", db_file=None, user_kpoints_settings=None, nstruc = 60,):
     """
-    Returns quasi-harmonic gibbs free energy workflow.
-    Note: phonopy package is required for the final analysis step if qha_type="phonopy"
     Args:
-        structure (Structure): input structure.
-        deformations (list): list of deformation matrices(list of lists).
+        structure_prim (Structure): input structure ASE Structure object.
         vasp_input_set_relax (VaspInputSet)
         vasp_input_set_static (VaspInputSet)
         vasp_cmd (str): vasp command to run.
         db_file (str): path to the db file.
         user_kpoints_settings (dict): example: {"grid_density": 7000}
-        t_step (float): temperature step (in K)
-        t_min (float): min temperature (in K)
-        t_max (float): max temperature (in K)
-        mesh (list/tuple): reciprocal space density
-        eos (str): equation of state used for fitting the energies and the volumes.
-            options supported by phonopy: "vinet", "murnaghan", "birch_murnaghan".
-            Note: pymatgen supports more options than phonopy. see pymatgen.analysis.eos.py
-        qha_type(str): quasi-harmonic approximation type: "debye_model" or "phonopy",
-            default is "debye_model"
-        pressure (float): in GPa
-        poisson (float): poisson ratio
+	
     Returns:
         Workflow
     """
     tag = datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S-%f')
+    # Build a 3-layer Al slab with vacancy in on-top configuration
+    pri = fcc111('Al', size=(1,1,3))
+    add_adsorbate(pri,'X',1.7,'ontop')
+    pri.center(vacuum=10.0, axis=2)
+
+    # Build the parent lattice and a 4x4 supercell
+    platt = ParentLattice(pri, site_symbols=[['Al'],['Al'],['Al','Na'],['X','O']])
+    platt.get_sublattice_types(pretty_print = True) # Print info regarding the ParentLattice object
+    scell = SuperCell(platt,[4,4])
+
+    # Collect 60 random structures in a StructuresSet object
+    sset = StructuresSet(platt)
+    nstruc = 60
+
+    for i in range(nstruc):
+        concentration = {0:[randint(0,4*4)],1:[randint(0,4*4)]} # Pick a random concentration of "Na" substitutions and "O" adsorbants
+    	sset.add_structure(scell.gen_random(concentration)) # Generate and add a random structure to the StructuresSet
+
+    print("\nRandom structures (first 3):")
+    sset.write_to_db("sset.json") # Write JSON db file for visualization with ASE's GUI.
+    
+
 
     # get the input set for the optimization and update it if we passed custom settings
     vis_relax = vasp_input_set or MPRelaxSet(structure, force_gamma=True)
@@ -49,34 +60,6 @@ def wf_gibbs_free_energy(structure, deformations, vasp_input_set_relax=None, vas
     fws = [OptimizeFW(structure=structure, vasp_input_set=vis_relax, vasp_cmd=vasp_cmd,
                       db_file=db_file, name="{} structure optimization".format(tag))]
 
-    # get the input set for the static calculations and update it if we passed custom settings
-    uis_static = {"ISIF": 2, "ISTART":1}
-    lepsilon = False # use IBRION = -1; don't update the ions
-    if qha_type not in ['debye model']:
-        uis_static = {'ISIF'}
-        lepsilon = True # use IBRION = -8; DFPT
-    vis_static = MPStaticSet(structure, force_gamma=True, lepsilon=lepsilon,
-                         user_kpoints_settings=user_kpoints_settings,
-                         user_incar_settings=uis_static)
-
-    # create each deformation Firework and add them to the Fireworks list
-    parents = fws[0]
-    deformations = [Deformation(defo_mat) for defo_mat in deformations]
-    for n, deformation in enumerate(deformations):
-        fw = TransmuterFW(name="{} {} {}".format(tag, 'gibbs deformation', n), structure=structure,
-                          transformations=['DeformStructureTransformation'],
-                          transformation_params=[{"deformation": deformation.tolist()}],
-                          vasp_input_set=vis_static, parents=parents,
-                          vasp_cmd=vasp_cmd, db_file=db_file)
-        fws.append(fw)
-
-    parents = fws[1:] # all of the deformation Fireworks
-    if qha_type not in ["debye_model"]:
-        from phonopy import Phonopy
-    fw_analysis = Firework(GibbsFreeEnergyTask(tag=tag, db_file=db_file, t_step=t_step, t_min=t_min,
-                                               t_max=t_max, mesh=mesh, eos=eos, qha_type=qha_type,
-                                               pressure=pressure, poisson=poisson),
-                           name="gibbs free energy", parents=parents)
     fws.append(fw_analysis)
 
     # finally, create the workflow
